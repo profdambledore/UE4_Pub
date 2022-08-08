64d6d5cea7007a3e2e371f77282be5c40b3dbcc8
Dan <43572775+profdambledore@users.noreply.github.com>
2022-08-08 18:26:24 +0100

    v0.0 - The Start
    Added Base Unreal Files
    Enabled Plugins
    Added Synty assets to .gitignore

---
+++ Source/UE4_Pub/UE4_PubGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_PubGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_PUB_API AUE4_PubGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
